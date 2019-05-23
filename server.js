d93aaa3fc25f5123a47735db6f02c84278fcb504
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-22 23:22:55 -0400

    updated ports

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // API and HTML routes
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
+// require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
