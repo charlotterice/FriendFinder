4852e1650d72f4f47ba57d8e037a601064fa3f34
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-22 23:25:25 -0400

    reverting back

--- server.js
+++ server.js
@@ -1,42 +1,18 @@
-// var express = require("express");
-// var path = require("path");
-
-// var app = express();
-// var PORT = process.env.PORT || 8080;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-
-// app.get("/", function(req, res) {
-//     res.sendfile(path.join(__dirname, "public/home.html"));
-//   });
-
-// app.listen(PORT, function() {
-//     // Log (server-side) when our server has started
-//     console.log(PORT);
-//   });
-
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
-// For serving of static CSS
-app.use(express.static(__dirname + "/app/css"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// API and HTML routes
-// require("./app/routing/apiRoutes.js")(app);
-// require("./app/routing/htmlRoutes.js")(app);
+app.get("/", function(req, res) {
+    res.sendfile(path.join(__dirname, "public/home.html"));
+  });
 
 app.listen(PORT, function() {
-	console.log("App listening on PORT: " + PORT);
-});
+    // Log (server-side) when our server has started
+    console.log(PORT);
+  });
