ed283734485814532fef1eb59fc0c6093a922cff
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-25 01:12:08 -0400

    added lots of data

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [
+    {
+        name: "Olivia Dunham",
+        photo: 
+        scores []
+    },
+    {
+        name: "Walter Bishop",
+        photo:
+        scores[]
+    },
+    {
+        name: "Peter Bishop",
+        photo:
+        scores: []
+    },
+    {
+        name: "Phillip Broyles",
+        photo:
+        scores:[]
+    },
+    {
+        name: "Nina Sharp",
+        photo:
+        scores:[]
+    }
+]
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -11,6 +12,12 @@ app.use(express.json());
 app.get("/", function(req, res) {
     res.sendfile(path.join(__dirname, "public/home.html"));
   });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:application/vnd.api+json}));
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
