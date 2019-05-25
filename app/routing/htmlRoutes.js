6fe4127209d75d1dcb13a8e308f94fbfc8726416
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-25 02:45:04 -0400

    quick commit while everything is actually working again

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 module.exports=function(app){
     app.get("/survey",function(request, response){
-        response.sendFile(path.join(_dirname,"./app/public/survey.html"));
+        response.sendFile(path.join(__dirname,"../public/survey.html"));
     });
 
     app.use(function(request,response){
-        response.sendFile(path.join(_dirname, "./app/public/home.html"));
+        response.sendFile(path.join(__dirname, "./public/home.html"));
     })
 }
